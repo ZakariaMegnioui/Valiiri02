79c0677b4469f0b3327d4aed4bc4ba84dc597f9c
zakaria_megnioui <ziko10sporty@gmail.com>
2022-05-03 17:26:47 +0000

    deploy

--- src/views/Theard-Page.vue
+++ src/views/Theard-Page.vue
@@ -23,7 +23,7 @@
 </div>
   </div>
   <!-- component -->
-  <div class="testimony py-16 " >  
+  <div class="testimony py-16" >  
     <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
         <h2 class="mb-12 text-center text-2xl text-gray-900 font-bold md:text-4xl">What's my team say</h2>
         <div class="grid gap-8 md:grid-rows-2 lg:grid-cols-2">
