2ccb0e54fb44a108ea9f51e9561847ccd1f9595b
zakaria_megnioui <ziko10sporty@gmail.com>
2022-05-03 17:23:41 +0000

    tetimony card color

--- src/views/Theard-Page.vue
+++ src/views/Theard-Page.vue
@@ -23,11 +23,11 @@
 </div>
   </div>
   <!-- component -->
-  <div class="testimony py-16 " style="background:#eff0f4;">  
+  <div class="testimony py-16 " >  
     <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
         <h2 class="mb-12 text-center text-2xl text-gray-900 font-bold md:text-4xl">What's my team say</h2>
         <div class="grid gap-8 md:grid-rows-2 lg:grid-cols-2">
-          <div style="background-color: #E5E6EB; display: grid; place-items: center;" class="topOne row-span-2 p-6 border border-gray-100 rounded-xl  text-center sm:p-8" >
+          <div style=" display: grid; place-items: center;" class="topOne bg-gradient-to-r from-gray-300 via-neutral-200 to-slate-200 shadow-md row-span-2 p-6 border border-gray-100 rounded-xl  text-center sm:p-8" >
             <div class="h-full flex flex-col justify-center space-y-4">
               <img class="w-20 h-20 mx-auto rounded-full" :src="require(`@/assets/img/amine.png`)" alt="user avatar" height="220" width="220" loading="lazy">
               <p class="text-gray-600 md:text-xl"> <span class="font-serif">"</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat repellat perspiciatis excepturi est. Non ipsum iusto aliquam consequatur repellat provident, omnis ut, sapiente voluptates veritatis cum deleniti repudiandae ad doloribus. <span class="font-serif">"</span></p>
@@ -38,7 +38,7 @@
             </div>
           </div>
 
-          <div  style="background-color: #E5E6EB; display: grid; place-items: center;" class="middelOne p-6 border border-gray-100 rounded-xl  sm:flex sm:space-x-8 sm:p-8">
+          <div  style=" display: grid; place-items: center;" class="middelOne bg-gradient-to-r from-gray-300 via-neutral-200 to-slate-200 shadow-md p-6 border border-gray-100 rounded-xl  sm:flex sm:space-x-8 sm:p-8">
             <img class="w-20 h-20 mx-auto rounded-full" :src="require(`@/assets/img/ali.png`)" alt="user avatar" height="220" width="220" loading="lazy">
             <div class="space-y-4 mt-4 text-center sm:mt-0 sm:text-left">
               <p class="text-gray-600"> <span class="font-serif">"</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat repellat perspiciatis excepturi est. Non ipsum iusto aliquam consequatur repellat provident, omnis ut, sapiente voluptates veritatis cum deleniti repudiandae ad doloribus. <span class="font-serif">"</span></p>
@@ -48,7 +48,7 @@
               </div>
             </div>
           </div>
-          <div style="background-color: #E5E6EB; display: grid; place-items: center;" class="lastOne p-6 border border-gray-100 rounded-xl  sm:flex sm:space-x-8 sm:p-8">
+          <div style=" display: grid; place-items: center;" class="lastOne  shadow-md bg-gradient-to-r from-gray-300 via-neutral-200 to-slate-200 p-6 border border-gray-100 rounded-xl  sm:flex sm:space-x-8 sm:p-8">
             <img class="w-20 h-20 mx-auto rounded-full" :src="require(`@/assets/img/mobarak.png`)" alt="user avatar" height="220" width="220" loading="lazy">
             <div class="space-y-4 mt-4 text-center sm:mt-0 sm:text-left">
               <p class="text-gray-600"> <span class="font-serif">"</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat repellat perspiciatis excepturi est. Non ipsum iusto aliquam consequatur repellat provident, omnis ut, sapiente voluptates veritatis cum deleniti repudiandae ad doloribus. <span class="font-serif">"</span></p>
